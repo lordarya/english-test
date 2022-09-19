240ee5c2ff15064a4c90bcdaaf953073ea11b75a
lordarya <12aryacandrayana@gmail.com>
2022-09-19 10:58:55 +0800

    :smile: create model

---
+++ app/Models/Jawaban.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jawaban extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'jawaban',
+        'is_true',
+        'id_soal',
+    ];
+}
